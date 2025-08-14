38cf1251968ac1588275596c9474a8a8d7ccd475
Pablo Cardozo <cardozopabloj@gmail.com>
2025-08-14 07:58:23 -0300

    test02

--- clase 02 12-08-25/Main.java
+++ clase 02 12-08-25/Main.java
@@ -1,6 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== FIN DEL SISTEMA DE MEMBRESÍAS Y ACTIVIDADES ===");
+        
     }
 }
 
+class Medallon {
+    protected String nombre;
+    protected int precio;
+    
+    public Medallon(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+class Ingrediente {
+    protected String nombre;
+    protected int precio;
+    
+    public Ingrediente(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+class Pan {
+    protected String nombre;
+    protected int precio;
+    
+    public Pan(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+class Hamburguesa {
+    protected Medallon medallones;
+    protected Ingrediente ingredientes;
+    protected Pan pan;
+
+    public Hamburguesa(Medallon medallones, Ingrediente ingredientes, Pan pan) {
+        this.medallones = medallones;
+        this.ingredientes = ingredientes;
+        this.pan = pan;
+    }
+}
